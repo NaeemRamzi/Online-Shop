85df38de7e6cd931e81961ddeb3cd8d708a35a51
Naeem Ramzi <naeemramzi2@gmail.com>
2022-06-17 20:29:04 +0430

    Added TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+  return <h1 className="text-2xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
